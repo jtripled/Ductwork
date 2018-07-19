6d9ffbcc9961aa770b50cac04173eaf7fb51472c
jtripled <jtripled@gmail.com>
2018-07-19 12:05:36 -0500

    remove voxen dependency

--- src/main/java/com/jtripled/ductwork/DuctworkBlocks.java
+++ src/main/java/com/jtripled/ductwork/DuctworkBlocks.java
@@ -5,8 +5,6 @@ import com.jtripled.ductwork.block.BlockItemDuct;
 import com.jtripled.ductwork.tile.TileFilterHopper;
 import com.jtripled.ductwork.tile.TileItemDuct;
 import net.minecraft.block.Block;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemBlock;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -32,18 +30,5 @@ public class DuctworkBlocks
         GameRegistry.registerTileEntity(TileItemDuct.class, ITEM_DUCT.getRegistryName().toString());
         
         Ductwork.getProxy().registerIgnoredProperties(FILTER_HOPPER, BlockFilterHopper.ENABLED);
-    }
-    
-    @SubscribeEvent
-    public static void onRegisterItems(RegistryEvent.Register<Item> event)
-    {
-        Item FILTER_HOPPER_ITEM = (new ItemBlock(FILTER_HOPPER)).setRegistryName(FILTER_HOPPER.getRegistryName()).setUnlocalizedName(FILTER_HOPPER.getUnlocalizedName());
-        Item ITEM_DUCT_ITEM = (new ItemBlock(ITEM_DUCT)).setRegistryName(ITEM_DUCT.getRegistryName()).setUnlocalizedName(ITEM_DUCT.getUnlocalizedName());
-        
-        event.getRegistry().register(FILTER_HOPPER_ITEM);
-        event.getRegistry().register(ITEM_DUCT_ITEM);
-        
-        Ductwork.getProxy().registerItemRenderer(FILTER_HOPPER_ITEM, "normal");
-        Ductwork.getProxy().registerItemRenderer(ITEM_DUCT_ITEM, "normal");
     }
 }
---
+++ src/main/java/com/jtripled/ductwork/DuctworkItems.java
@@ -0,0 +1,28 @@
+package com.jtripled.ductwork;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+/**
+ *
+ * @author jtripled
+ */
+@Mod.EventBusSubscriber
+public class DuctworkItems
+{
+    public static final Item FILTER_HOPPER_ITEM = (new ItemBlock(DuctworkBlocks.FILTER_HOPPER)).setRegistryName(DuctworkBlocks.FILTER_HOPPER.getRegistryName()).setUnlocalizedName(DuctworkBlocks.FILTER_HOPPER.getUnlocalizedName());
+    public static final Item ITEM_DUCT_ITEM = (new ItemBlock(DuctworkBlocks.ITEM_DUCT)).setRegistryName(DuctworkBlocks.ITEM_DUCT.getRegistryName()).setUnlocalizedName(DuctworkBlocks.ITEM_DUCT.getUnlocalizedName());
+    
+    @SubscribeEvent
+    public static void onRegisterItems(RegistryEvent.Register<Item> event)
+    {
+        event.getRegistry().register(FILTER_HOPPER_ITEM);
+        event.getRegistry().register(ITEM_DUCT_ITEM);
+        
+        Ductwork.getProxy().registerItemRenderer(FILTER_HOPPER_ITEM, "normal");
+        Ductwork.getProxy().registerItemRenderer(ITEM_DUCT_ITEM, "normal");
+    }
+}

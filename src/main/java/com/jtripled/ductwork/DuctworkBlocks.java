7b8933a50f667d591f21231628e7f0ddefa27d8e
jtripled <jtripled@gmail.com>
2018-08-03 11:01:36 -0500

    prep for 1.13

--- src/main/java/com/jtripled/ductwork/DuctworkBlocks.java
+++ src/main/java/com/jtripled/ductwork/DuctworkBlocks.java
@@ -10,22 +10,24 @@ import net.minecraft.tileentity.TileEntity;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry.ObjectHolder;
 
 /**
  *
  * @author jtripled
  */
 @Mod.EventBusSubscriber
+@ObjectHolder(Ductwork.ID)
 public class DuctworkBlocks
 {
-    public static final BlockFilterHopper FILTER_HOPPER = new BlockFilterHopper();
-    public static final BlockItemDuct ITEM_DUCT = new BlockItemDuct();
+    public static final BlockFilterHopper FILTER_HOPPER = null;
+    public static final BlockItemDuct ITEM_DUCT = null;
     
     @SubscribeEvent
     public static void onRegisterBlocks(RegistryEvent.Register<Block> event)
     {
-        register(event, FILTER_HOPPER, TileFilterHopper.class, BlockFilterHopper.ENABLED);
-        register(event, ITEM_DUCT, TileItemDuct.class);
+        register(event, new BlockFilterHopper(), TileFilterHopper.class, BlockFilterHopper.ENABLED);
+        register(event, new BlockItemDuct(), TileItemDuct.class);
     }
     
     private static void register(RegistryEvent.Register<Block> event, Block block, Class<? extends TileEntity> tileClass, IProperty... ignoredProperties)
